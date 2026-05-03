e284a05374b7e662ccb461a2356797cf2c4b0c35
manojkumar <vijaymanoj0000@gmail.com>
2026-05-03 16:46:52 +0530

    profile image fix

--- src/components/HeroProfileImage.tsx
+++ src/components/HeroProfileImage.tsx
@@ -239,7 +239,7 @@ const HeroProfileImage = memo(() => {
           <img
             src={profileImg}
             alt="profile"
-            className="w-full h-full object-contain"
+            className="w-full h-full object-cover object-[center_20%]"
           />
         </div>
       </div>
